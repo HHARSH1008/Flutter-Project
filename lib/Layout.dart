9f6ed8061ffe03361af2a80e93e38b4c08d88688
HHARSH1008 <harshporiya03@gmail.com>
2026-07-17 10:03:54 +0530

    First Commit

---
+++ lib/Layout.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+
+// class Layout extends StatelessWidget{
+//  const Layout({super.key});
+
+//   @override
+//     Widget build(BuildContext context) {
+//         return Scaffold(
+//             body:Column(
+//                 children: [
+//                     height:200,
+//                     width:100,
+//                     Decoration:BoxDe
+//                 ],
+//             ),//column       // child:Text(""),
+
+//             );
+        
+//     }
+// }
